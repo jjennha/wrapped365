50d20621cfb1acc3b445eb43e7bba1718b488bd5
Jenny <jha@iastate.edu>
2020-03-21 16:25:39 -0500

    config update

--- src/config_dev.js
+++ src/config_dev.js
@@ -3,7 +3,7 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "66b496dd7646458a874a51e419b66742";
 export const clientSecret = "3534b3ae53554ff7a3926ee2216bcf82";
-export const redirectUri = "https://master.d2xjerelp5g7cr.amplifyapp.com/";
+export const redirectUri = "http://localhost:3000/";
 export const scopes = [
     "user-top-read",
     "user-read-currently-playing",
